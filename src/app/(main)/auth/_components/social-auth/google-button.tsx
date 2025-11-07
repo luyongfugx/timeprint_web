e6930f658493228c02d2fb6097c6a496e10aa6bf
waynelu <luyongfugx@163.com>
2025-11-07 08:30:08 +0800

    google btn

--- src/app/(main)/auth/_components/social-auth/google-button.tsx
+++ src/app/(main)/auth/_components/social-auth/google-button.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { siGoogle } from "simple-icons";
 
 import { SimpleIcon } from "@/components/simple-icon";
@@ -7,18 +7,18 @@ import { cn } from "@/lib/utils";
 import { supabase } from "@/lib/supabaseClient";
 // import { useTranslation } from "@/contexts/translation-context"
 import { useState } from "react";
-import { useRouter } from "next/navigation"
-import { toast } from "@/hooks/use-toast"
+import { useRouter } from "next/navigation";
+import { toast } from "@/hooks/use-toast";
 export function GoogleButton({ className, ...props }: React.ComponentProps<typeof Button>) {
   // const { t, locale } = useTranslation()
-  const [loading, setLoading] = useState(false)
-  const router = useRouter()
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
   const handleGoogleSignIn = async () => {
-    setLoading(true)
+    setLoading(true);
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: "http://localhost:3000/api/auth/callback", // 回调地址
+        redirectTo: "https://team.timeprint.net/api/auth/callback", // 回调地址
       },
     });
 
@@ -27,16 +27,16 @@ export function GoogleButton({ className, ...props }: React.ComponentProps<typeo
         title: "loginPage.googleSignInFailed",
         description: error.message,
         variant: "destructive",
-      })
+      });
     } else {
       toast({
         title: "loginPage.signInSuccess",
-        description:"loginPage.signInSuccessDescription",
-      })
-      router.push(`/dashboard/default`)
-      router.refresh()
+        description: "loginPage.signInSuccessDescription",
+      });
+      router.push(`/dashboard/default`);
+      router.refresh();
     }
-    setLoading(false)
+    setLoading(false);
   };
 
   return (
